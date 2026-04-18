655d7e58ad9f20aedbfd1c68d69d8fbeeb54be08
Greg Bishop <ggb667@gmail.com>
2026-04-18 00:20:02 -0400

    Added getToken.sh

---
+++ scripts/getToken.sh
@@ -0,0 +1,3 @@
+curl -sS https://partner.instinctvet.com/v1/auth/token -H 'Content-Type: application/x-www-form-urlencoded'
+19:53:56
+ --data-urlencode 'grant_type=client_credentials' --data-urlencode 'client_id=935f6041-2a97-434f-b686-03953f1dca71' --data-urlencode 'client_secret=Vf+5JpsXT0q/HpiKrqkJMyl9C6u1l7/vRyJs9aL9lyookMT2tGLc6DhdivLqXkZI'
